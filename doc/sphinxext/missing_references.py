30953e60d1645ef1709bae08e976a97368a638d2
Alex Rudy <alex.rudy@gmail.com>
2019-07-15 20:53:56 -0400

    Add location to missing references

--- doc/sphinxext/missing_references.py
+++ doc/sphinxext/missing_references.py
@@ -18,6 +18,8 @@ The basic operation is:
 import json
 import os.path
 
+from docutils.utils import get_source_line
+
 
 def record_missing_reference_handler(app, env, node, contnode):
     if not app.config.missing_references_write_json:
@@ -25,16 +27,43 @@ def record_missing_reference_handler(app, env, node, contnode):
         return
 
     if not hasattr(env, "missing_reference_record"):
-        env.missing_reference_record = []
+        env.missing_reference_record = set()
     record = env.missing_reference_record
 
     domain = node["refdomain"]
     typ = node["reftype"]
     target = node["reftarget"]
+    location = get_location(node, app)
 
     dtype = "{}:{}".format(domain, typ)
 
-    record.append([dtype, target])
+    # nitpick_ignore won't use the location field, but we store it anyways
+    # to be helpful for those who want to make the documentation better.
+    # It is included first so that the sort order of these records groups
+    # local missing references.
+    record.add((location, dtype, target))
+
+
+def get_location(node, app):
+    (path, line) = get_source_line(node)
+
+    if path:
+
+        basepath = os.path.abspath(os.path.join(app.confdir, ".."))
+        path = os.path.relpath(path, start=basepath)
+
+        if path.startswith(os.path.pardir):
+            path = os.path.join("<external>", os.path.basename(path))
+
+    else:
+        path = "<unknown>"
+
+    if line:
+        line = str(line)
+    else:
+        line = ""
+
+    return "%s:%s" % (path, line)
 
 
 def save_missing_references_handler(app, exc):
@@ -43,8 +72,12 @@ def save_missing_references_handler(app, exc):
         return
 
     path = os.path.join(app.confdir, "missing-references.json")
+
+    records = [list(r) for r in app.env.missing_reference_record]
+    records.sort()
+
     with open(path, "w") as stream:
-        json.dump(app.env.missing_reference_record, stream)
+        json.dump(records, stream, indent=2)
 
 
 def prepare_missing_references_handler(app, config):
@@ -56,9 +89,12 @@ def prepare_missing_references_handler(app, config):
         return
 
     with open(path, "r") as stream:
-        nitpick_ignore = json.load(stream)
+        data = json.load(stream)
 
-    config.nitpick_ignore.extend(tuple(item) for item in nitpick_ignore)
+    # We store lists of (location, dtype, reference) in the JSON file for easy
+    # cataloging, but nitpick_ignore only wants (dtype, referece) tuples to
+    # ignore references.
+    config.nitpick_ignore.extend((item[-2], item[-1]) for item in data)
 
 
 def setup(app):
